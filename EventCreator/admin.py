6e0741bd3582960c6702358de53418b2141362c4
Konstantin Uvarov <konstantin.uvar@gmail.com>
2018-10-28 17:09:40 +0700

    - enable model editing throw AdminSite

--- EventCreator/admin.py
+++ EventCreator/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Artist, ArtistClass, Decoration, Event, EventLocation, LocationAddress, OrderInfo
+
 # Register your models here.
+admin.site.register(Artist)
+admin.site.register(ArtistClass)
+admin.site.register(Decoration)
+admin.site.register(Event)
+admin.site.register(EventLocation)
+admin.site.register(LocationAddress)
+
+admin.site.register(OrderInfo)
