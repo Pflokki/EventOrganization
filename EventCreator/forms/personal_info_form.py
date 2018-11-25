98b30cf4efec5eec84bbb148ada423a38fa4f50d
Konstantin Uvarov <konstantin.uvar@gmail.com>
2018-11-25 14:59:36 +0700

    - added event_time label

--- EventCreator/forms/personal_info_form.py
+++ EventCreator/forms/personal_info_form.py
@@ -6,4 +6,4 @@ class PersonalInfoForm(forms.Form):
     p_last_name = forms.CharField(label="Фамилия", max_length=100)
     p_phone = forms.CharField(label="Номер телефона", max_length=100)
     p_email = forms.EmailField(label="E-mail", max_length=100)
-    event_time = forms.DateField(input_formats=['%Y-%m-%d'])
+    event_time = forms.DateField(label="Дата мероприятия", input_formats=['%Y-%m-%d'])
